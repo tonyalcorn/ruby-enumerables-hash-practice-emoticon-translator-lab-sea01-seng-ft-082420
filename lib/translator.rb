2687c2d64375a2156b4928e84c6b6895df03d4ec
Anthony Alcorn <xmastony@gmail.com>
2020-08-19 03:55:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,19 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+
+def load_library(path)
+  emoticons = YAML.load_file(path)
+  
+  emoticon_hash = {}
+  
+  emoticons.do each |feeling, character|
+  binding.pry
 end
+
+
+
 
 def get_japanese_emoticon
   # code goes here
@@ -11,3 +22,17 @@ end
 def get_english_meaning
   # code goes here
 end
+
+#endemoticons.each_with_object({}) do |(key, value), final_array|
+    
+   # value.each do |inner_key, inner_value|
+     # inner_value = :english, :japanese
+     # if !final_array[key][inner_value]
+       # final_array[key][inner_value]
+      #  binding.pry
+      #end
+     # binding.pry
+    #end
+    #binding.pry
+  #end
+  #binding.pry
